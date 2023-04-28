b6ddae058afbd3ac5d4bbb36f4af5af67b17d3bc
surbhi-chaturvedi <surbhi70029@gmail.com>
2023-04-28 12:11:39 +0530

    structuring the code

--- client/script.js
+++ client/script.js
@@ -10,10 +10,10 @@ function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
+        
         element.textContent += '.';
 
-        // If the loading indicator has reached three dots, reset it
+        
         if (element.textContent === '....') {
             element.textContent = '';
         }
@@ -33,9 +33,7 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
+
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -67,23 +65,23 @@ const handleSubmit = async (e) => {
 
     const data = new FormData(form)
 
-    // user's chatstripe
+    
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    // to clear the textarea input 
+    
     form.reset()
 
-    // bot's chatstripe
+    
     const uniqueId = generateUniqueId()
     chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    // to focus scroll to the bottom 
+    
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
+    
     const messageDiv = document.getElementById(uniqueId)
 
-    // messageDiv.innerHTML = "..."
+    
     loader(messageDiv)
 
     const response = await fetch('https://ouraichatbot.onrender.com/', {
@@ -101,7 +99,7 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+        const parsedData = data.bot.trim() 
 
         typeText(messageDiv, parsedData)
     } else {
